f3e0cf87a3ec49d9b977c8479e3d469d63da9a23
Quishia Desiree Famador <httpsqorus@gmail.com>
2025-11-12 09:32:39 +0400

    folder deletion
    - deleted the resources folder and transferred the resource files in their respective exercise folders

---
+++ Assessment 1 - Skills Portfolio/Exercise 2 - Alexa Tell Me a Joke/AlexaTellMeAJoke.py
@@ -0,0 +1,22 @@
+"""
+# Exercise 2 - Alexa tell me a Joke
+
+The randomJokes.txt file in the resources folder contains a dataset of random jokes. 
+Each joke is on a new line and consists of a setup and punchline separated by a question mark. 
+For example:
+
+    - Why did the chicken cross the road? To get to the other side.
+    - What happens if you boil a clown? You get a laughing stock.
+  
+Develop a Tkinter GUI application that acts like a joke-telling assistant. 
+The program should:
+
+- Display a window with a button labeled "Alexa tell me a Joke".
+- When the button is clicked, randomly select a joke from the randomJokes.txt file, 
+    display the setup of the joke in a label.
+- Provide another button labeled "Show Punchline"- When clicked, 
+    display the punchline below the setup.
+- Include a "Next Joke" button so the user can request another random joke.
+- Additionally , provide a "Quit" button to close the application.
+"""
+
